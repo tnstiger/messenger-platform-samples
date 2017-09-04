04b68827e3bff51a0141bddf8019614fe684d2db
gino <gino@emq.com>
2017-09-04 16:21:06 +0800

    no message

--- app.js
+++ app.js
@@ -80,6 +80,11 @@ app.get('/webhook', function(req, res) {
  * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
  *
  */
+ app.get('/setup',function(req,res){
+
+     setupGetStartedButton(res);
+ });
+
 app.post('/webhook', function (req, res) {
   var data = req.body;
 
@@ -916,6 +921,32 @@ function removePersistentMenu(){
 })
 }
 
+function setupGetStartedButton(res){
+     var messageData = {
+             "get_started":[
+             {
+                 "payload":"USER_DEFINED_PAYLOAD"
+                 }
+             ]
+     };
+     // Start the request
+     request({
+         url: 'https://graph.facebook.com/v2.6/me/messenger_profile?access_token='+ PAGE_ACCESS_TOKEN,
+         method: 'POST',
+         headers: {'Content-Type': 'application/json'},
+         form: messageData
+     },
+     function (error, response, body) {
+         if (!error && response.statusCode == 200) {
+             // Print out the response body
+             res.send(body);
+
+         } else {
+             // TODO: Handle errors
+             res.send(body);
+         }
+     });
+ }
 
 // Start server
 // Webhooks must be available via SSL with a certificate signed by a valid
