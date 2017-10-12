1c4ceca1fae5e4ff3694d95c15d868392aa88803
curt <curt6815463@gmail.com>
2017-10-13 01:28:44 +0800

    fix port to 80

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const
   exec = require('child_process').exec,
   fs = require('fs');
 var app = express();
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 80);
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(express.static('public'));
